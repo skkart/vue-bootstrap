d374eb3e77c3b1eae1805cd10556ed84bf8e5a49
skkart <skkarthik10@gmail.com>
2019-07-01 20:22:26 +0530

    Include imports

---
+++ index.js
@@ -0,0 +1,10 @@
+export {default as directivesPopOver} from './directive/pop-over'
+export {default as directivesTooltip} from './directive/tooltip'
+export {default as AppModal} from './components/AppModal'
+export {default as AppModalConfirm} from './components/AppModalConfirm'
+export {default as AppBootstrapDatepicker} from './components/AppBootstrapDatepicker'
+export {default as BootstrapSelect} from './components/BootstrapMultiSelect'
+
+export default {
+  version: '1.0.0'
+}
