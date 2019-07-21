fbd7d16847b49e70f6137e7269e7533a9cb486d4
skkart <skkarthik10@gmail.com>
2019-07-21 20:45:17 +0530

    Updated index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export {default as directivesTooltip} from './directive/tooltip'
 export {default as AppModal} from './components/AppModal'
 export {default as AppModalConfirm} from './components/AppModalConfirm'
 export {default as AppBootstrapDatepicker} from './components/AppBootstrapDatepicker'
-export {default as BootstrapSelect} from './components/BootstrapMultiSelect'
+export {default as AppBootstrapSelect} from './components/BootstrapMultiSelect'
 
 export default {
   version: '1.0.0'
