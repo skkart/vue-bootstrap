fb551e1fd7c7ea672fde75dfa7899cb4a55e8f99
skkart <skkarthik10@gmail.com>
2020-01-26 13:49:37 +0530

    Updated Index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export {default as directivesPopOver} from './directive/pop-over'
 export {default as directivesTooltip} from './directive/tooltip'
+export {default as directivesDateRange} from './directive/bootstrap-date-range'
 export {default as AppModal} from './components/AppModal'
 export {default as AppBootstrapDatepicker} from './components/AppBootstrapDatepicker'
 export {default as AppBootstrapSelect} from './components/BootstrapMultiSelect'
