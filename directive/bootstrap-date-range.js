063f74a4aa2f2cf3ba9ca9cd36efc6036130a14b
skkart <skkarthik10@gmail.com>
2020-01-26 13:26:13 +0530

    Added Daterange directive

---
+++ directive/bootstrap-date-range.js
@@ -0,0 +1,38 @@
+export default {
+  inserted (el, binding) {
+    if (!binding.value.pickerOptions) {
+      binding.value.pickerOptions = {}
+    }
+    $(el).daterangepicker({
+      timePicker: true,
+      timePicker24Hour: true,
+      timePickerIncrement: 1,
+      buttonClasses: 'btn btn-sm',
+      applyButtonClasses: 'btn-apply',
+      cancelClass: 'btn-cancel',
+      opens: 'center',
+      showDropdowns: true,
+      linkedCalendars: false,
+      maxSpan: 'off',
+      ...binding.value.pickerOptions
+    }, (...args) => {
+      binding.value.onDateSubmit(...args)
+    })
+
+  },
+  update (el, binding) {
+    if (binding.value.pickerOptions) {
+      const datePicker = $(el).data('daterangepicker')
+      datePicker.ranges = binding.value.pickerOptions.ranges
+      datePicker.startDate = binding.value.pickerOptions.startDate
+      datePicker.endDate = binding.value.pickerOptions.endDate
+      datePicker.maxDate = binding.value.pickerOptions.maxDate
+      datePicker.updateView()
+      datePicker.updateElement()
+    }
+  },
+  unbind (el, binding) {
+    const datePicker = $(el).data('daterangepicker')
+    datePicker.remove()
+  }
+}
